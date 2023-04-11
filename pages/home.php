61ee597ba1d0fc8397197110f2e2da8c2b239b2d
Walker Silvestre <walker_contas@hotmail.com>
2023-04-11 17:40:34 -0300

    Update home.php

--- pages/home.php
+++ pages/home.php
@@ -2,6 +2,9 @@
 
 <p>Teste visual php</p>
 
+<p>Teste de acesso do Cloud Prix:</p>
+<a href="http://192.168.1.34:80/zAPI_Test/001/dl/gs1/01/00000000020325?11=230411&17=230413&3922=000140&3952=002690">Clique aqui</a>
+
 <div class="white-space"></div>
 
 <?php
